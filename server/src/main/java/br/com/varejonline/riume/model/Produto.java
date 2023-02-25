42a239757e8428260eb57dc08ec32e51714e93c3
RiumeCarlosA <riumecarlos@hotmail.com>
2023-02-25 22:10:17 +0000

    Ajustando models

--- server/src/main/java/br/com/varejonline/riume/model/Produto.java
+++ server/src/main/java/br/com/varejonline/riume/model/Produto.java
@@ -1,6 +1,7 @@
 package br.com.varejonline.riume.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,9 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.DynamicUpdate;
 
+import br.com.varejonline.riume.exception.MovimentacaoInvalid;
+import br.com.varejonline.riume.model.enums.Movimentos;
 import lombok.Data;
 
 @Data
@@ -36,6 +40,15 @@ public class Produto implements Serializable {
 	@ManyToOne
 	private Estoque estoque;
 	
+	@OneToMany(mappedBy = "produto")
+//	@JoinTable(name = "tb_produto_movimentacao", 
+//			   joinColumns = @JoinColumn(name = "produto_id"), 
+//			   inverseJoinColumns = @JoinColumn(name = "movimentacao_id"))
+	private List<Movimentacao> movimentacoes;
+	
+	@Column(name = "saldo_inicial")
+	private Integer saldoInicial;
+	
 	@Column(name = "deleted", columnDefinition = "boolean default false")
 	protected boolean deleted = false;
 
@@ -45,6 +58,24 @@ public class Produto implements Serializable {
 		this.qtdMin = qtdMin;
 	}
 	
+	public Produto(String nome, String codBarra, Integer qtdMin, Integer saldoInicial, Movimentacao movimentacao) {
+		this.nome = nome;
+		this.codBarra = codBarra;
+		this.qtdMin = qtdMin;
+		this.setSaldoInicial(saldoInicial);
+		movimentacao.addMovimento(Movimentos.SALDO_INICIAL);
+		this.movimentacoes.add(movimentacao);
+	}
+	
+	public void setSaldoInicial(Integer saldo) {
+		if(saldo < 0) {
+			new MovimentacaoInvalid("error.produto.saldo.saldo-menor-zero");
+		}
+		if(saldo < this.qtdMin) {
+			new MovimentacaoInvalid("error.produto.saldo.saldo-menor-qtd-minima");
+		}
+		this.saldoInicial = saldo;
+	}
 	
 	
 }
