83e3a970ff9ca74638a455f151c37fd7c99961c8
RiumeCarlosA <riumecarlos@hotmail.com>
2023-02-26 03:24:59 +0000

    Configurando login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  {
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/models/credenciais.ts
@@ -0,0 +1,4 @@
+export interface Credenciais {
+    usuario: string;
+    senha: string;
+}
