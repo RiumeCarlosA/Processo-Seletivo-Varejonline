e92a7b1d331d457c6e5c46c17f3382ef932d45f3
RiumeCarlosA <riumecarlos@hotmail.com>
2023-02-26 01:35:50 +0000

    configurando spring security e jwt

--- server/src/main/java/br/com/varejonline/riume/config/TestConfig.java
+++ server/src/main/java/br/com/varejonline/riume/config/TestConfig.java
@@ -3,10 +3,12 @@ package br.com.varejonline.riume.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import br.com.varejonline.riume.service.config.DBService;
 
 @Configuration
+@Profile("test")
 public class TestConfig {
 
 	@Autowired
